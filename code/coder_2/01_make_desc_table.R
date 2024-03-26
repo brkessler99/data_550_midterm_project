a24d071fa3cba17f4215407cc9b46a980415bcb4
Stephanie <saponce@emory.edu>
2024-03-26 15:53:31 -0400

    generate descriptive table 1

---
+++ code/coder_2/01_make_desc_table.R
@@ -0,0 +1,32 @@
+here::i_am("code/coder_2/01_make_desc_table.R")
+absolute_path_to_data <- here::here("clean_data/wastewater_clean.csv")
+data <- read.csv(absolute_path_to_data, header = TRUE)
+
+library(kableExtra, dplyr)
+
+#mean percentile avg per year by region
+mean_percentile_year_region <- data |>
+  group_by(year, region) |>
+  rename(Region = region) |>
+  summarize(mean_percentile = mean(mean_percentile, na.rm = TRUE))
+
+#mean percentile avg per year
+mean_percentile_year_overall <- data |>
+  group_by(year) |>
+  summarize(mean_percentile = mean(mean_percentile, na.rm = TRUE)) |>
+  mutate(Region = "Overall")
+
+#combined table
+combined_data <- bind_rows(mean_percentile_year_region, mean_percentile_year_overall)
+
+desc_table <- combined_data |>
+  pivot_wider(names_from = year, values_from = mean_percentile, names_prefix = "Year_", values_fill = NA) |>
+  rename_with(~sub("Year_", "", .x), starts_with("Year_")) |>
+  arrange(Region != "Overall") |>
+  kable(digits = 1) |>
+  kable_styling(bootstrap_options = "striped", full_width = F, position = "left")
+
+saveRDS(
+  desc_table,
+  file = here::here("output/desc_table.Rds")
+)
