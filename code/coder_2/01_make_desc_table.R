8acf5000268e18f6cd0040699346a490af128c53
Suprithi Pingle <suprithi.pingle@emory.edu>
2024-04-14 14:08:16 -0400

    added caption to table

--- code/coder_2/01_make_desc_table.R
+++ code/coder_2/01_make_desc_table.R
@@ -23,7 +23,7 @@ desc_table <- combined_data |>
   pivot_wider(names_from = year, values_from = mean_percentile, names_prefix = "Year_", values_fill = NA) |>
   rename_with(~sub("Year_", "", .x), starts_with("Year_")) |>
   arrange(Region != "Overall") |>
-  kable(digits = 1) |>
+  kable(digits = 1,caption = "Table 1: SARS-CoV-2 virus level mean percentile by US region from 2020-2023") |>
   kable_styling(bootstrap_options = "striped", full_width = F, position = "left")
 
 saveRDS(
